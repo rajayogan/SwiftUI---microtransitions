fc5499a1cfe70b66325632e3c32477de817b5cdd
Rajayogan <rajayogan27@gmail.com>
2025-06-05 21:43:49 +0530

    Initial Commit

---
+++ microtransitions/microtransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  microtransitionsApp.swift
+//  microtransitions
+//
+//  Created by Rajayogan on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct microtransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ microtransitionsTests/microtransitionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  microtransitionsTests.swift
+//  microtransitionsTests
+//
+//  Created by Rajayogan on 05/06/25.
+//
+
+import Testing
+@testable import microtransitions
+
+struct microtransitionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
